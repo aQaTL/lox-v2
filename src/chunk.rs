2def7d17295a436813c5244881ebc836e58a3ce3
aQaTL <mmsoltys@outlook.com>
2023-08-20 11:53:33 +0200

    Restart the implementation

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,5 +1,3 @@
-use crate::memory;
-use crate::value::{Value, ValueArray};
 use std::fmt::{Display, Formatter};
 use std::ptr;
 
@@ -48,72 +46,6 @@ impl TryFrom<u8> for OpCode {
 			Err(UnknownOpCode(value))
 		} else {
 			unsafe { Ok(std::mem::transmute::<u8, OpCode>(value)) }
-		}
-	}
-}
-
-pub struct Chunk {
-	pub count: usize,
-	pub capacity: usize,
-	pub code: *mut u8,
-
-	pub lines: *mut u32,
-	pub constants: ValueArray,
-}
-
-impl Default for Chunk {
-	fn default() -> Self {
-		Chunk {
-			count: 0,
-			capacity: 0,
-			code: ptr::null_mut(),
-
-			lines: ptr::null_mut(),
-			constants: ValueArray::default(),
-		}
-	}
-}
-
-impl Chunk {
-	pub fn init(chunk: *mut Chunk) {
-		unsafe {
-			(*chunk) = Chunk::default();
-		}
-	}
-
-	pub fn write(chunk: *mut Chunk, byte: u8, line: u32) {
-		unsafe {
-			if (*chunk).capacity < (*chunk).count + 1 {
-				let old_capacity = (*chunk).capacity;
-				(*chunk).capacity = memory::grow_capacity(old_capacity);
-
-				(*chunk).code =
-					memory::grow_array::<u8>((*chunk).code, old_capacity, (*chunk).capacity);
-
-				(*chunk).lines =
-					memory::grow_array::<u32>((*chunk).lines, old_capacity, (*chunk).capacity);
-			}
-
-			*((*chunk).code.add((*chunk).count)) = byte;
-			*((*chunk).lines.add((*chunk).count)) = line;
-			(*chunk).count += 1;
-		}
-	}
-
-	/// Adds `value` to `Chunk.constants` and returns it's index
-	pub fn add_constant(chunk: *mut Chunk, value: Value) -> u8 {
-		unsafe {
-			ValueArray::write(&mut (*chunk).constants, value);
-			((*chunk).constants.count - 1).try_into().unwrap()
-		}
-	}
-
-	pub fn free(chunk: *mut Chunk) {
-		unsafe {
-			memory::free_array::<u8>((*chunk).code, (*chunk).capacity);
-			memory::free_array::<u32>((*chunk).lines, (*chunk).capacity);
-			ValueArray::free(&mut (*chunk).constants);
-			Chunk::init(chunk)
 		}
 	}
 }
